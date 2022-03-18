8157b0ceff66aa440ef5d218916b3a4f071fd637
lourdessaa11 <saa21333@uvg.edu.gt>
2022-03-17 21:13:07 -0600

    Pruebas Unitarias

--- HDT6/src/FactoryMapsTest.java
+++ HDT6/src/FactoryMapsTest.java
@@ -1,8 +1,23 @@
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class FactoryMapsTest {
-    @Test
-    public void testCreateMap() {
+import org.junit.jupiter.api.Test;
 
-    }
+import java.util.*;
+
+class FactoryMapsTest {
+
+	FactoryMaps factory = new FactoryMaps();
+	HashMap<String, String> hashMap = new HashMap<String, String>();
+	TreeMap<String, String> treeMap = new TreeMap<String, String>();
+	LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<String, String>();
+	
+	@Test
+	void testCreateMap() {
+		
+		assertEquals(hashMap.getClass(),factory.createMap(1).getClass());
+		assertEquals(treeMap.getClass(),factory.createMap(2).getClass());
+		assertEquals(linkedHashMap.getClass(),factory.createMap(3).getClass());
+		
+	}
+
 }
